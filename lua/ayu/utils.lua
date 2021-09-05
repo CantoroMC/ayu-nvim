fecbbc44bb5d3329fb8e4cb5093b87ccf859a0ed
Marco Cantoro <marco.cantoro92@outlook.it>
2021-09-05 05:11:00 +0200

    inverted order of utils.highlight

--- lua/ayu/utils.lua
+++ lua/ayu/utils.lua
@@ -1,14 +1,14 @@
 local utils = {}
 
 function utils.highlight(group, color)
-  local style = color.style and 'gui='   .. color.style or 'gui=NONE'
   local fg    = color.fg    and 'guifg=' .. color.fg    or 'guifg=NONE'
   local bg    = color.bg    and 'guibg=' .. color.bg    or 'guibg=NONE'
+  local style = color.style and 'gui='   .. color.style or 'gui=NONE'
   local sp    = color.sp    and 'guisp=' .. color.sp    or ''
 
   vim.api.nvim_command(string.format(
     'highlight %s %s %s %s %s',
-    group, style, fg, bg, sp
+    group, fg, bg, style, sp
   ))
 end
 
